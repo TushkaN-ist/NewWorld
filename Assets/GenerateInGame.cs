b39a61bd7c0b79b892c72ca9da22b056643679e5
Kardini <Kardini@yandex.ru>
2016-12-16 22:41:06 +0400

    Added String
    We need it, dude.

--- Assets/GenerateInGame.cs
+++ Assets/GenerateInGame.cs
@@ -10,6 +10,7 @@ public class GenerateInGame : MonoBehaviour {
     [Range(1f,45f)]
     public float percentDropTrash;
     public float arroundStick=3;
+	public string Tushka="Fucking Cool FOX GUY";
 
     Program program;
 	// Use this for initialization
